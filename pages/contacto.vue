e40be32999c478e9e73044641aae44a831ef1a0e
rcluis <raulcatalanluis@gmail.com>
2023-02-14 21:50:22 +0100

    feat(seo): add contact and seo info

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -12,12 +12,27 @@
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { useSeoMeta, useTagTitle } from '@unhead/vue'
+import { STATICS_CDN } from '~/constants/urls'
 
 const { t } = useI18n({ useScope: 'local', inheritLocale: true })
+
+useTagTitle(t('seo.title'))
+useSeoMeta({
+  description: t('seo.description'),
+  ogDescription: t('seo.description'),
+  ogTitle: t('seo.title'),
+  ogImage: `${STATICS_CDN}website/seo/og-image.png`,
+  twitterCard: 'summary_large_image'
+})
 </script>
 <i18n>
 {
   "es": {
+    "seo": {
+      "title": "Contacta con los expertos en quiropráctica - ColumnaQuiro",
+      "description": "Contacta con nuestros quiroprácticos altamente capacitados para programar una cita personalizada y descubre cómo podemos ayudarte a recuperar tu calidad de vida."
+    },
     "title": "Contacta con ColumnaQuiro",
     "subtitle": "Centro Quiropráctico en Valencia"
   }
