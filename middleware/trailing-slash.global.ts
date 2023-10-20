5a1b2b32d294c05c0b7b004591ad94cf12ddf8a7
rcluis <raulcatalanluis@gmail.com>
2023-10-20 09:38:20 +0200

    removed: remove middleware slash

--- middleware/trailing-slash.global.ts
+++ middleware/trailing-slash.global.ts
@@ -1,13 +1,13 @@
-import { RouteLocationPathRaw } from 'vue-router'
-
-export default function ({ path, query, hash }: RouteLocationPathRaw) {
-  if (path === '/' || !path.endsWith('/')) {
-    return
-  }
-
-  const nextPath = path.replace(/\/+$/, '') || '/'
-  const nextRoute = { path: nextPath, query, hash }
-
-  // 308 Permanent Redirect
-  return navigateTo(nextRoute, { redirectCode: 308 })
-}
+// import { RouteLocationPathRaw } from 'vue-router'
+//
+// export default function ({ path, query, hash }: RouteLocationPathRaw) {
+//   if (path === '/' || !path.endsWith('/')) {
+//     return
+//   }
+//
+//   const nextPath = path.replace(/\/+$/, '') || '/'
+//   const nextRoute = { path: nextPath, query, hash }
+//
+//   // 308 Permanent Redirect
+//   return navigateTo(nextRoute, { redirectCode: 308 })
+// }
