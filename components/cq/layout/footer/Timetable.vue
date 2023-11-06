6bbc3d6239436364b769ed51fbeb4d4cd2b4febf
rcluis <raulcatalanluis@gmail.com>
2023-11-06 20:45:50 +0100

    changes: timetable to the footer

--- components/cq/layout/footer/Timetable.vue
+++ components/cq/layout/footer/Timetable.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="md:text-start text-center">
-    <div v-for="timetable in timetableItems" :key="rt(timetable.day)">
-      <div class="text-lg font-medium">
-        {{ rt(timetable.day ) }}
-      </div>
-      <div class="text-base pl-4">
-        {{ rt(timetable.schedule ) }}
+  <div>
+    <div class="text-lg font-medium text-center pb-4">
+      {{ t('general.layout.footer.timetable.title') }}
+    </div>
+    <div class="flex flex-row justify-center align-bottom text-start flex-wrap">
+      <div v-for="timetable in timetableItems" :key="rt(timetable.day)" class="mr-4">
+        <div class="text-base font-medium">
+          {{ rt(timetable.day ) }}
+        </div>
+        <div class="text-base">
+          {{ rt(timetable.schedule ) }}
+        </div>
       </div>
     </div>
   </div>
@@ -13,7 +18,12 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 
-const { tm, rt } = useI18n()
+interface TimetableItems {
+  day: string
+  schedule: string
+}
 
-const timetableItems = tm('general.layout.footer.timetable')
+const { tm, rt, t } = useI18n()
+
+const timetableItems: TimetableItems[] = tm('general.layout.footer.timetable.items')
 </script>
