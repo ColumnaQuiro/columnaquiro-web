373fecfe9c4106097620bb14b8fab8d68435d513
rcluis <raulcatalanluis@gmail.com>
2023-11-25 21:29:02 +0100

    fixed: seo locales

--- pages/para-quien-sirve-la-quiropractica/[type].vue
+++ pages/para-quien-sirve-la-quiropractica/[type].vue
@@ -13,13 +13,14 @@ import type { UrlEnPersonType, UrlEsPersonType } from '~/types/Person'
 const route = useRoute()
 const { t, locale } = useI18n()
 const useSeo = useSEO()
-const personType: string | undefined = typeOfPersonToKey.get(route.params.type as string)
+const type = route.params.type as string
+const personType: string | undefined = typeOfPersonToKey.get(type)
 const personTitle = t(`chiropracticForWhom.types.personTitle.${personType}`)
 
 if (personType) {
   route.meta.nuxtI18n = {
-    en: { type: locale.value === 'en' ? personType : typeOfPersonLocales.en[personType as UrlEsPersonType] },
-    es: { type: locale.value === 'es' ? personType : typeOfPersonLocales.es[personType as UrlEnPersonType] }
+    en: { type: locale.value === 'en' ? type : typeOfPersonLocales.en[type as UrlEsPersonType] },
+    es: { type: locale.value === 'es' ? type : typeOfPersonLocales.es[type as UrlEnPersonType] }
   }
 }
 
