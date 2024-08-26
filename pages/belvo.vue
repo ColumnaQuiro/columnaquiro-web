13c4fa7a390c9991a7a751956d753c07015c27b5
rcluis <raulcatalanluis@gmail.com>
2024-08-26 12:03:07 +0200

    added: belvo test

---
+++ pages/belvo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Redirecting to Belvo universal link
+  </div>
+</template>
+
+<script setup lang="ts">
+window.setTimeout(() => window.open('https://columnaquiro.com/test', '_self'), 2000)
+</script>
